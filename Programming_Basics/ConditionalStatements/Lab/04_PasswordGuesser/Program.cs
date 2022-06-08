44fa01e2a88b5e7b52d317607eaf0475c6337d21
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-08 16:07:05 +0300

    Lab

---
+++ Programming_Basics/ConditionalStatements/Lab/04_PasswordGuesser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04_PasswordGuesser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            if (str.Equals("s3cr3t!P@ssw0rd"))
+            {
+                Console.WriteLine("Welcome");
+            }
+            else 
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
