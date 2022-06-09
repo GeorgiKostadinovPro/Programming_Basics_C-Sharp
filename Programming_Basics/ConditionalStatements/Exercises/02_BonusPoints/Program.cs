5bd4e50706b966c452ae220c0e121412baa3283b
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-09 15:24:38 +0300

    Add files via upload

---
+++ Programming_Basics/ConditionalStatements/Exercises/02_BonusPoints/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02_BonusPoints
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+
+            double bonus = 0.0D;
+
+            if (start <= 100)
+            {
+                bonus += 5;
+            }
+            else if (start > 100 && start <= 1000)
+            {
+                bonus += 0.2 * start;
+            }
+            else if (start > 1000)
+            {
+                bonus += 0.1 * start;
+            }
+
+            if (start % 2 == 0)
+            {
+                bonus += 1;
+            }
+
+            if (start.ToString().EndsWith("5"))
+            {
+                bonus += 2;
+            }
+
+            Console.WriteLine(bonus);
+            Console.WriteLine(start + bonus);
+        }
+    }
+}
