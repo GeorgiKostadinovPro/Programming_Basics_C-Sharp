06ea228853a48e433cef88b474835b506af01e8a
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-11 14:38:16 +0300

    Lab

---
+++ Programming_Basics/FirstStepsIntoCoding/Lab/05_GreetingsByName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _05_GreetingsByName
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
