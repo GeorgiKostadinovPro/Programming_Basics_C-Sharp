d0be17ea757a42381b74cbb4a8a9d61d91a4cd61
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-14 22:17:37 +0300

    Add files via upload

---
+++ Programming_Basics/ConditionalStatementsAdvanced/Exercises/01_Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01_Cinema
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string cinema = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            int full = rows * columns;
+            double income = 0.0;
+            switch (cinema)
+            {
+                case "Premiere":
+                    income = full * 12.00;
+                    Console.WriteLine($"{income:F2} leva");
+                    break;
+                case "Normal":
+                    income = full * 7.50;
+                    Console.WriteLine($"{income:F2} leva");
+                    break;
+                case "Discount":
+                    income = full * 5.00;
+                    Console.WriteLine($"{income:F2} leva");
+                    break;
+            }
+        }
+    }
+}
