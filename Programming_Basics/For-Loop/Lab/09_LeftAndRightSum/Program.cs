922c06be631e5e2851b16519ee0ef819471af43c
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-15 20:44:53 +0300

    Add files via upload

---
+++ Programming_Basics/For-Loop/Lab/09_LeftAndRightSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09_LeftAndRightSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int rightSum = 0;
+            int leftSum = 0;
+
+            for (int i = 0; i < n * 2; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (i >= n)
+                {
+                    leftSum += number;
+                }
+                else 
+                {
+                    rightSum += number;
+                }
+            }
+
+            if (rightSum == leftSum)
+            {
+                Console.WriteLine($"Yes, sum = {rightSum}");
+            }
+            else 
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(rightSum - leftSum)}");
+            }
+        }
+    }
+}
