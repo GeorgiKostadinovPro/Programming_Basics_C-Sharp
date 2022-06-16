1bc66966cc1dcaaec7cd5bb50ae78ef7be220189
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-16 23:05:47 +0300

    Add files via upload

---
+++ Programming_Basics/For-Loop/Exercise/01_NumbersEndingIn7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01_NumbersEndingIn7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                  Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
