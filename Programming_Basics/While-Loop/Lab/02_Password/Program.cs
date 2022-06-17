8b826613dfe64726b8fd2ef89e3ea40229e26884
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-17 23:07:01 +0300

    Add files via upload

---
+++ Programming_Basics/While-Loop/Lab/02_Password/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02_Password
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            string guesser = Console.ReadLine();
+
+            while (guesser != password)
+            {
+                guesser = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {username}!");
+        }
+    }
+}
