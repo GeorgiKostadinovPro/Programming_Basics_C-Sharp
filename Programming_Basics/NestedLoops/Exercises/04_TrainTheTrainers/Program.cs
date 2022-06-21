8fa3ab3d05a143648abb61b205771e471c26bada
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-21 12:18:51 +0300

    Add files via upload

---
+++ Programming_Basics/NestedLoops/Exercises/04_TrainTheTrainers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04_TrainTheTrainers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int jury = int.Parse(Console.ReadLine());
+
+            string line = Console.ReadLine();
+
+            double finalResult = 0.0;
+            int gradeCount = 0;
+
+            while (line != "Finish")
+            {
+                string name = line;
+                double sum = 0.0;
+
+                for (int i = 0; i < jury; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    gradeCount++;
+                    sum += grade;
+                }
+
+                double avg = sum / jury;
+                Console.WriteLine($"{name} - {avg:f2}.");
+                finalResult += sum;
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Student's final assessment is {(finalResult / gradeCount):f2}.");
+        }
+    }
+}
