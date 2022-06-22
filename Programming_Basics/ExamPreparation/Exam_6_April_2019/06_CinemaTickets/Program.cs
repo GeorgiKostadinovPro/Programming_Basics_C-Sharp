4bfd6bb864fdfb24354e150f1186e2382fadbd45
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-22 20:03:57 +0300

    Add files via upload

---
+++ Programming_Basics/ExamPreparation/Exam_6_April_2019/06_CinemaTickets/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _06_CinemaTickets
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int students = 0;
+            int old = 0;
+            int kids = 0;
+
+            while (name != "Finish")
+            {
+                int freeSeats = int.Parse(Console.ReadLine());
+                int people = 0;
+
+                for (int i = 0; i < freeSeats; i++)
+                {
+                    string input = Console.ReadLine();
+
+                    if (input == "student")
+                    {
+                        students++;
+                    }
+                    else if (input == "kid")
+                    {
+                        kids++;
+                    }
+                    else if (input == "standard")
+                    {
+                        old++;
+                    }
+                    else if (input == "End")
+                    {
+                        break;
+                    }
+                    people++;
+                }
+
+                Console.WriteLine($"{name} - {(people * 1.0 / freeSeats * 100):f2}% full.");
+                name = Console.ReadLine();
+            }
+
+            int total = old + kids + students;
+            Console.WriteLine($"Total tickets: {total}");
+            Console.WriteLine($"{(students * 1.0 / total * 100):f2}% student tickets.");
+            Console.WriteLine($"{(old * 1.0 / total * 100):f2}% standard tickets.");
+            Console.WriteLine($"{(kids * 1.0 / total * 100):f2}% kids tickets.");
+        }
+    }
+}
