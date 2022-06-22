122a88b9fd765899874f93d5eab460901e0b18d0
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-22 20:02:51 +0300

    Add files via upload

---
+++ Programming_Basics/ExamPreparation/Exam_6_April_2019/03_OscarsWeekInCinema/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _03_OscarsWeekInCinema
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string type = Console.ReadLine();
+            int ticketsCount = int.Parse(Console.ReadLine());
+
+            double totalSum = 0.0D;
+
+            if (type == "normal")
+            {
+                if (name == "A Star Is Born")
+                {
+                    totalSum = ticketsCount * 7.50;
+                }
+                else if (name == "Bohemian Rhapsody")
+                {
+                    totalSum = ticketsCount * 7.35;
+                }
+                else if (name == "Green Book")
+                {
+                    totalSum = ticketsCount * 8.15;
+                }
+                else if (name == "The Favourite")
+                {
+                    totalSum = ticketsCount * 8.75;
+                }
+            }
+            else if (type == "luxury")
+            {
+                if (name == "A Star Is Born")
+                {
+                    totalSum = ticketsCount * 10.50;
+                }
+                else if (name == "Bohemian Rhapsody")
+                {
+                    totalSum = ticketsCount * 9.45;
+                }
+                else if (name == "Green Book")
+                {
+                    totalSum = ticketsCount * 10.25;
+                }
+                else if (name == "The Favourite")
+                {
+                    totalSum = ticketsCount * 11.55;
+                }
+            }
+            else if (type == "ultra luxury")
+            {
+                if (name == "A Star Is Born")
+                {
+                    totalSum = ticketsCount * 13.50;
+                }
+                else if (name == "Bohemian Rhapsody")
+                {
+                    totalSum = ticketsCount * 12.75;
+                }
+                else if (name == "Green Book")
+                {
+                    totalSum = ticketsCount * 13.25;
+                }
+                else if (name == "The Favourite")
+                {
+                    totalSum = ticketsCount * 13.95;
+                }
+            }
+
+            Console.WriteLine($"{name} -> {totalSum:f2} lv.");
+        }
+    }
+}
