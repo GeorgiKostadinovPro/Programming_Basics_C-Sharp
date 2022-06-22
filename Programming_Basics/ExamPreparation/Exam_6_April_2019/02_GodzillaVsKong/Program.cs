e8ca324ba1fb081b87033aeb2bac6f0472da6a3c
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-22 20:02:09 +0300

    Add files via upload

---
+++ Programming_Basics/ExamPreparation/Exam_6_April_2019/02_GodzillaVsKong/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02_GodzillaVsKong
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int people = int.Parse(Console.ReadLine());
+            double pricePerPerson = double.Parse(Console.ReadLine());
+
+            double decoration = budget * 0.10;
+            double clothesSum = people * pricePerPerson;
+            double total = 0;
+            double neededLeft = 0;
+            double promotion = 0;
+
+            if (people > 150)
+            {
+                promotion = clothesSum * 0.10;
+                neededLeft = clothesSum - promotion;
+                total = neededLeft + decoration;
+            }
+            else
+            {
+                clothesSum = people * pricePerPerson;
+                total = clothesSum + decoration;
+            }
+
+            if (total > budget)
+            {
+                double needed = total - budget;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {needed:f2} leva more.");
+            }
+            else
+            {
+                double left = budget - total;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {left:f2} leva left.");
+            }
+        }
+    }
+}
