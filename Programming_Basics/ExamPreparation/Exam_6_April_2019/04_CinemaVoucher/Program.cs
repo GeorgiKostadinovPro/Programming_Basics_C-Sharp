9725601f4181d07475e4719a086c762ff6596edf
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-22 20:03:19 +0300

    Add files via upload

---
+++ Programming_Basics/ExamPreparation/Exam_6_April_2019/04_CinemaVoucher/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _04_CinemaVoucher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int vaucher = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int tickets = 0;
+            int otherStuff = 0;
+
+            string name = Console.ReadLine();
+
+            while (name != "End")
+            {
+
+                if (name == "End")
+                {
+                    break;
+                }
+
+                if (name.Length > 8)
+                {
+                    int one = name[0];
+                    int two = name[1];
+                    sum = one + two;
+                    if (vaucher - sum < 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        tickets++;
+                        vaucher -= sum;
+                    }
+                }
+                else
+                {
+                    int one1 = name[0];
+                    sum = one1;
+                    if (vaucher - sum < 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        otherStuff++;
+                        vaucher -= sum;
+                    }
+                }
+
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine(tickets);
+            Console.WriteLine(otherStuff);
+        }
+    }
+}
