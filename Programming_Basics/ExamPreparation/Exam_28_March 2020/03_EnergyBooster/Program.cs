46cdd917e4364b9be81fb3c2e6b0bb6ee3058a5e
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-23 23:09:03 +0300

    Add files via upload

---
+++ Programming_Basics/ExamPreparation/Exam_28_March 2020/03_EnergyBooster/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _03_EnergyBooster
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string size = Console.ReadLine();
+            int orderSets = Int32.Parse(Console.ReadLine());
+
+            double fruitPrice = 0;
+            double sizeNumber = 0;
+
+            if (fruit == "Watermelon")
+            {
+                if (size == "small")
+                {
+                    sizeNumber = 2;
+                    fruitPrice = 56;
+                }
+                else if (size == "big")
+                {
+                    sizeNumber = 5;
+                    fruitPrice = 28.7;
+                }
+            }
+            else if (fruit == "Mango")
+            {
+                if (size == "small")
+                {
+                    sizeNumber = 2;
+                    fruitPrice = 36.66;
+                }
+                else if (size == "big")
+                {
+                    sizeNumber = 5;
+                    fruitPrice = 19.6;
+                }
+            }
+            else if (fruit == "Pineapple")
+            {
+                if (size == "small")
+                {
+                    sizeNumber = 2;
+                    fruitPrice = 42.1;
+                }
+                else if (size == "big")
+                {
+                    sizeNumber = 5;
+                    fruitPrice = 24.8;
+                }
+            }
+            else if (fruit == "Raspberry")
+            {
+                if (size == "small")
+                {
+                    sizeNumber = 2;
+                    fruitPrice = 20;
+                }
+                else if (size == "big")
+                {
+                    sizeNumber = 5;
+                    fruitPrice = 15.2;
+                }
+            }
+            double result = sizeNumber * fruitPrice * orderSets;
+
+            if (result >= 400 && result <= 1000)
+            {
+                result *= 0.85;
+            }
+            else if (result > 1000)
+            {
+                result *= 0.5;
+            }
+
+            Console.WriteLine($"{result:f2} lv.");
+        }
+    }
+}
