3d705c82f79dff7241767026ef44c469d664d811
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-24 14:52:39 +0300

    Add files via upload

---
+++ Programming_Basics/MyRegularExam13November2021/05_SuitcasesLoad/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _05_SuitcasesLoad
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double capacityOfLuggagePlace = double.Parse(Console.ReadLine());
+            string input;
+            int suitcase = 0;
+            int loadedSuitcase = 0;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                suitcase++;
+                double volumeOfLuggage = double.Parse(input);
+                if ((suitcase % 3 == 0) && (capacityOfLuggagePlace < volumeOfLuggage * 1.1))
+                {
+                    break;
+                }
+
+                if ((suitcase % 3 != 0) && (capacityOfLuggagePlace < volumeOfLuggage))
+                {
+                    break;
+                }
+
+                if (suitcase % 3 == 0)
+                {
+                    capacityOfLuggagePlace -= volumeOfLuggage * 0.1;
+                }
+
+                capacityOfLuggagePlace -= volumeOfLuggage;
+                loadedSuitcase++;
+            }
+
+            if (input == "End")
+            {
+                Console.WriteLine($"Congratulations! All suitcases are loaded!");
+            }
+            else
+            {
+                Console.WriteLine("No more space!");
+            }
+
+            Console.WriteLine($"Statistic: {loadedSuitcase} suitcases loaded.");
+        }
+    }
+}
