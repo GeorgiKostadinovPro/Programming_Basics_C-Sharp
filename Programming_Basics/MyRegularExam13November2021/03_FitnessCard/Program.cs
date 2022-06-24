c15833e409ff56f18a6299324d2faf53ee09c5cb
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-24 14:52:26 +0300

    Add files via upload

---
+++ Programming_Basics/MyRegularExam13November2021/03_FitnessCard/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace _03_FitnessCard
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double haveMoney = double.Parse(Console.ReadLine());
+            char sex = char.Parse(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
+            string sport = Console.ReadLine();
+
+            double cardPrice = 0.0D;
+
+            if (sex == 'm')
+            {
+                if (sport == "Gym")
+                {
+                    cardPrice = 42;
+
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Boxing")
+                {
+                    cardPrice = 41;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Yoga")
+                {
+                    cardPrice = 45;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Zumba")
+                {
+                    cardPrice = 34;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Dances")
+                {
+                    cardPrice = 51;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Pilates")
+                {
+                    cardPrice = 39;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+            }
+            else if (sex == 'f')
+            {
+                if (sport == "Gym")
+                {
+                    cardPrice = 35;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Boxing")
+                {
+                    cardPrice = 37;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Yoga")
+                {
+                    cardPrice = 42;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Zumba")
+                {
+                    cardPrice = 31;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Dances")
+                {
+                    cardPrice = 53;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+                else if (sport == "Pilates")
+                {
+                    cardPrice = 37;
+                    if (age <= 19)
+                    {
+                        cardPrice *= 0.80;
+                    }
+                }
+            }
+
+            if (haveMoney >= cardPrice)
+            {
+                Console.WriteLine($"You purchased a 1 month pass for {sport}.");
+            }
+            else
+            {
+                double difference = cardPrice - haveMoney;
+                Console.WriteLine($"You don't have enough money! You need ${difference:f2} more.");
+           }
+        }
+    }
+}
---
+++ Programming_Basics/MyRegularExam13November2021/04_Balls/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace _04_Balls
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int balls = int.Parse(Console.ReadLine());
+            int red = 0;
+            int orange = 0;
+            int yellow = 0;
+            int white = 0;
+            int others = 0;
+            int blackDivides = 0;
+            int total = 0;
+
+            for (int i = 0; i < balls; i++)
+            {
+                string colour = Console.ReadLine();
+
+                if (colour == "red")
+                {
+                    total += 5;
+                    red++;
+                }
+                else if (colour == "orange")
+                {
+                    total += 10;
+                    orange++;
+                }
+                else if (colour == "yellow")
+                {
+                    total += 15;
+                    yellow++;
+                }
+                else if (colour == "white")
+                {
+                    total += 20;
+                    white++;
+                }
+                else if (colour == "black")
+                {
+                    total /= 2;
+                    blackDivides++;
+                }
+                else
+                {
+                    others++;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Total points: {total}");
+            sb.AppendLine($"Points from red balls: {red}");
+            sb.AppendLine($"Points from orange balls: {orange}");
+            sb.AppendLine($"Points from yellow balls: {yellow}");
+            sb.AppendLine($"Points from white balls: {white}");
+            sb.AppendLine($"Other colors picked: {others}");
+            sb.AppendLine($"Divides from black balls: {blackDivides}");
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
