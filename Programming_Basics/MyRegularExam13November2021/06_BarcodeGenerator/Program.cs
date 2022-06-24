cb100e95b25af29138258d4de89952d5643b7acc
GeorgiKostadinovPro <72508846+GeorgiKostadinovPro@users.noreply.github.com>
2022-06-24 14:52:52 +0300

    Add files via upload

---
+++ Programming_Basics/MyRegularExam13November2021/06_BarcodeGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06_BarcodeGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            var firstStart = (int)(first / 1000);
+            var secondStart = (int)(first / 100) % 10;
+            var thirdStart = (int)(first / 10) % 10;
+            var fourthStart = (int)(first % 10);
+
+            var firstEnd = (int)(second / 1000);
+            var secondEnd = (int)(second / 100) % 10;
+            var thirdEnd = (int)(second / 10) % 10;
+            var fourthEnd = (int)(second % 10);
+
+            for (int i = firstStart; i < firstEnd + 1; i++)
+            {
+                for (int j = secondStart; j < secondEnd + 1; j++)
+                {
+                    for (int k = thirdStart; k < thirdEnd + 1; k++)
+                    {
+                        for (int l = fourthStart; l < fourthEnd + 1; l++)
+                        {
+                            if (i % 2 != 0 && j % 2 != 0 && k % 2 != 0 && l % 2 != 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
